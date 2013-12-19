6fd4fb50c9f92de9e9caf5eebea90e111692732d
Mounier Florian <paradoxxx.zero@gmail.com>
2013-12-19 16:34:14 +0100

    Might be better to use_2to3 if there are print statements and python 3 compat.

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     include_package_data=True,
     platforms='any',
     install_requires=['pytest>=2.3'],
+    use_2to3=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
