a0313080e24261dd7c5dae9c9cb28c9ceab0510e
mk <manojklm@gmail.com>
2014-12-05 00:31:41 +0530

    Remove support for windows #46

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
         'Operating System :: MacOS :: MacOS X',
         'Topic :: Software Development :: Testing',
         'Topic :: Software Development :: Libraries',
