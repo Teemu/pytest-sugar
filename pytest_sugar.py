76278cc0cbdde464b4229f518014cdcf4d1a8b4f
Teemu <teemu.on.ihminen@gmail.com>
2016-12-27 20:18:10 +0200

    Remove deprecation warning that didn't match code

--- pytest_sugar.py
+++ pytest_sugar.py
@@ -156,11 +156,6 @@ def pytest_configure(config):
         sugar_reporter = SugarTerminalReporter(standard_reporter)
         config.pluginmanager.unregister(standard_reporter)
         config.pluginmanager.register(sugar_reporter, 'terminalreporter')
-    else:
-        print(colored(
-            'WARNING: --nosugar is deprecated, please use -p no:sugar instead',
-            'yellow'
-        ))
 
 
 def pytest_report_teststatus(report):
