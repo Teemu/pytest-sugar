4e190a8a2397ce53db414ed7a2fad9060939d7de
John Rusnak <john.j.rusnak@att.net>
2020-05-20 10:02:50 -0700

    Get test count from collection_finish hook over runtestloop

--- pytest_sugar.py
+++ pytest_sugar.py
@@ -73,7 +73,7 @@ def flatten(l):
             yield x
 
 
-def pytest_runtestloop(session):
+def pytest_collection_finish(session):
     reporter = session.config.pluginmanager.getplugin('terminalreporter')
     if reporter:
         reporter.tests_count = len(session.items)
