8fab31aa37195455373a3824b1d040f44138c903
Justin Mayer <entroP@gmail.com>
2023-04-10 13:50:08 +0200

    Drop official support for Python 3.7

--- setup.py
+++ setup.py
@@ -61,10 +61,10 @@ setup(
         "Topic :: Software Development :: Libraries",
         "Topic :: Utilities",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
 )
